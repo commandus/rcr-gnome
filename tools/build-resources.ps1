80df307a35195ee7f243516269871e1a59d14b07
Andrei Ivanov <andrei.i.ivanov@gmail.com>
2023-05-08 14:39:40 +0900

    Windows support

--- tools/build-resources.ps1
+++ tools/build-resources.ps1
@@ -1,9 +1,9 @@
 # glib-compile-resources
 $GLIB_COMPILE_RESOURCES = $($args[0])
 $CMAKE_CURRENT_SOURCE_DIR = $($args[1])
-Write-Output($CMAKE_CURRENT_SOURCE_DIR)
+Write-Output("*** $GLIB_COMPILE_RESOURCES $CMAKE_CURRENT_SOURCE_DIR")
 New-Item -ItemType Directory -Force -Path $CMAKE_CURRENT_SOURCE_DIR | Out-Null
-
+Set-Location $CMAKE_CURRENT_SOURCE_DIR
 $C="$GLIB_COMPILE_RESOURCES $CMAKE_CURRENT_SOURCE_DIR/rcr-gnome.resource.xml --target=$CMAKE_CURRENT_SOURCE_DIR/resources.c --generate-source"
 Write-Output($C)
 Invoke-expression $C
